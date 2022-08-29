24f9d8216817bbdcfdfe1a16d1d1ccf67608819b
Lucas Leme <lucasratm40@gmail.com>
2022-08-29 16:47:21 -0300

    Sidebar component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 
 import { Header } from './components/Header'
 import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
 
 import './globals.css'
+import styles from './App.module.css'
+
 
 export function App() {
 
@@ -10,14 +13,19 @@ export function App() {
     <div>
       <Header />
 
-      <Post 
-        author="Lucas"
-        content="Testing"
-      />
-      <Post 
-        author="João"
-        content="Testing 2"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Lucas"
+            content="Testing"
+          />
+          <Post 
+            author="João"
+            content="Testing 2"
+          />
+        </main>
+      </div>
     </div>
   )
 }

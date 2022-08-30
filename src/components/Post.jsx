2b918cb2f02d558a9ae5102fcb12afab1e135818
Lucas Leme <lucasratm40@gmail.com>
2022-08-30 12:46:28 -0300

    Comment form created

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,6 +34,20 @@ export function Post(){
           <a href="#">#nlw</a> {' '}
           <a href="#">#Rocketseat</a></p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder='Deixe um comentário'
+        />
+
+        <footer>
+          <button type='submit'>
+            Publicar
+          </button>
+        </footer>
+      </form>
     </article>
   )
 }
